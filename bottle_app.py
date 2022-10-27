bcbe63c35492fc152291d140bbd56f650e276936
priyankalemati17 <priyankalemati17@gmail.com>
2022-10-27 00:47:56 +0000

    topic-web-intro saved

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,6 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route, template
+from bottle import default_app, route,get,post, template, request,redirect
 import sqlite3
 
 
@@ -26,6 +26,45 @@ def get_list():
     rows = list(rows)
     rows = [ {'id':row[0], 'desc':row[1]} for row in rows]
     return template("shopping_list.tpl", name="shin" , shopping_list=rows )
+
+
+@get('/add')
+def get_add():
+    return template("add_item.tpl")
+
+@post('/add')
+def post_add():
+    descrption = request.forms.get("descrption")
+    cursor = connection.cursor()
+    cursor.execute(f"insert into list (descrption) values ('{descrption}')")
+    connection.commit()
+    redirect('/list')
+
+@route("/delete/<id>")
+def get_delete(id):
+    cursor = connection.cursor()
+    cursor.execute(f"delete from list where id={id}")
+    connection.commit()
+    redirect('/list')
+
+@get("/edit/<id>")
+def get_edit(id):
+    cursor = connection.cursor()
+    items = cursor.execute(f"select descrption from list where id={id}")
+    items = list(items)
+    if len(items) != 1:
+        redirect('/list')
+    descrption = items[0][0]
+    return template("edit_item.tpl", id=id, descrption=descrption)
+
+@post("/edit/<id>")
+def post_edit(id):
+    descrption = request.forms.get("descrption")
+    cursor = connection.cursor()
+    cursor.execute(f"update list set descrption='{descrption}' where id={id}")
+    connection.commit()
+    redirect('/list')
+
 
 
 application = default_app()
---
+++ topic-web-intro/views/edit_item.tpl
@@ -0,0 +1,12 @@
+<html>
+<body>
+Edit this item...
+<hr/>
+<form action="/edit/{{id}}" method="post">
+  <p>Edit Item:<input name="descrption" value="{{descrption}}"/></p>
+  <p><button type="submit">Submit</button></p>
+</form>
+<hr/>
+<a href="/list">Cancel</a>
+</body>
+</html>
